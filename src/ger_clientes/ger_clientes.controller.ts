575e9062137f02edc1fab02badc1385594b61edd
Roberto Luiz Veiga <portview.me@gmail.com>
2024-10-28 20:23:22 -0300

    feat: pequena mudança no Olá mundo - teste 13-

--- src/ger_clientes/ger_clientes.controller.ts
+++ src/ger_clientes/ger_clientes.controller.ts
@@ -9,7 +9,7 @@ export class GerClientesController {
 
     @Get('olaMundo')
     async olaMundo(): Promise<{ message: string }> {
-        return { message: "Olá mundo versão 00012 !" };
+        return { message: "Olá mundo versão 00013 !" };
     }
 
     @Get('conformidades')
